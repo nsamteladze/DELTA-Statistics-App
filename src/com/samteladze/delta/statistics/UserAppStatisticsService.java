1f299965850b5ef73b065b8cfbe0ee6541d0e5f5
Nikolai Samteladze <nikolay.samteladze@gmail.com>
2012-09-16 18:35:32 -0400

    Added service to collect app stats for user
    1. Added UserAppStatisticsService that collects statistics in
    UserFriendly format and saves it to a separate file (UserAppStat.txt).
    This service is invoked when users clicks Collect button in
    DeltaStatisticsActivity. Statistics is not sent to the server.

---
+++ src/com/samteladze/delta/statistics/UserAppStatisticsService.java
@@ -0,0 +1,30 @@
+package com.samteladze.delta.statistics;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.commonsware.cwac.wakeful.WakefulIntentService;
+import com.samteladze.delta.statistics.datamodel.StatisticsFormat;
+import com.samteladze.delta.statistics.utils.FileManager;
+import com.samteladze.delta.statistics.utils.LogManager;
+
+public class UserAppStatisticsService extends WakefulIntentService
+{	
+	public UserAppStatisticsService()
+	{
+		super("UserAppStatisticsService");
+	}
+
+	@Override
+	protected void doWakefulWork(Intent intent)
+	{
+		Context context = this.getApplicationContext();
+		
+		LogManager.Log(UserAppStatisticsService.class.getSimpleName(), "Starting the service");
+		
+        AppStatisticsProvider appStatisticsProvider = new AppStatisticsProvider(context);
+        appStatisticsProvider.CollectStatistics(); 
+        
+        FileManager.SaveStatistics(appStatisticsProvider.GetStatistics(StatisticsFormat.UserFriendly), FileManager.USER_APP_STAT_FILE_PATH, true);       
+	}
+}
